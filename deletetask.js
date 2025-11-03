17489c0a574d92b0d988a210c3b9ebd21fd6b792
kaiquedev027 <72511950+kaiquedev027@users.noreply.github.com>
2025-11-03 01:16:08 -0400

    Delete deletetask.js

--- deletetask.js
+++
@@ -1,8 +0,0 @@
-function deleteTask(id) {
-            if (confirm('Tem certeza que deseja deletar esta tarefa?')) {
-                tasks = tasks.filter(task => task.id !== id);
-                saveTasks();
-                renderTasks();
-                updateStats();
-            }
-        }
